cef1806f548a34e22422be3549b5a75194727fd7
Nguyen Trong Dat <trongdatngt@gmail.com>
2023-03-15 00:02:15 +0700

    update-connguoi-khachhang-danhsachkhachhang

---
+++ PHANMEMBANLINHKIENMAYTINH/src/model/ConNguoi.java
@@ -0,0 +1,66 @@
+package model;
+
+public class ConNguoi {
+private String ma;
+private String ten;
+private String SDT;
+private boolean gioiTinh;
+private String email;
+private String diaChi;
+public ConNguoi(String ma, String ten, String sDT, boolean gioiTinh, String email, String diaChi) {
+	this.ma = ma;
+	this.ten = ten;
+	this.SDT = sDT;
+	this.gioiTinh = gioiTinh;
+	this.email = email;
+	this.diaChi = diaChi;
+}
+
+public ConNguoi(String ma) {
+	this.ma = ma;
+}
+
+public String getMa() {
+	return ma;
+}
+public void setMa(String ma) {
+	this.ma = ma;
+}
+public String getTen() {
+	return ten;
+}
+public void setTen(String ten) {
+	this.ten = ten;
+}
+public String getSDT() {
+	return SDT;
+}
+public void setSDT(String sDT) {
+	SDT = sDT;
+}
+public boolean isGioiTinh() {
+	return gioiTinh;
+}
+public void setGioiTinh(boolean gioiTinh) {
+	this.gioiTinh = gioiTinh;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getDiaChi() {
+	return diaChi;
+}
+public void setDiaChi(String diaChi) {
+	this.diaChi = diaChi;
+}
+@Override
+public String toString() {
+	return "ConNguoi [ma=" + ma + ", ten=" + ten + ", SDT=" + SDT + ", gioiTinh=" + gioiTinh + ", email=" + email
+			+ ", diaChi=" + diaChi + "]";
+}
+
+
+}
--- PHANMEMBANLINHKIENMAYTINH/src/model/DanhSachKhanhHang.java
+++ PHANMEMBANLINHKIENMAYTINH/src/model/DanhSachKhanhHang.java
@@ -1,5 +1,69 @@
 package model;
 
-public class DanhSachKhanhHang {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+public class DanhSachKhanhHang {
+	ArrayList<KhachHang> dsKhachHang = new ArrayList<KhachHang>();
+
+	public DanhSachKhanhHang(ArrayList<KhachHang> dsKhachHang) {
+		this.dsKhachHang = dsKhachHang;
+	}
+
+public boolean themKhachHang(KhachHang kH) {
+	if(!dsKhachHang.contains(kH)) {
+		dsKhachHang.add(kH);
+		return true;
+	}
+	return false;
+}
+
+public boolean xoaKhachHang(String maKH) {
+	KhachHang kH=new KhachHang(maKH);
+	if(dsKhachHang.contains(kH)) {
+		dsKhachHang.remove(kH);
+		return true;
+	}
+	return false;
+}
+public boolean suaKhachHang(KhachHang kH) {
+	for(int i=0;i<dsKhachHang.size();++i) {
+		if(dsKhachHang.get(i).getMa().equals(kH.getMa())) {
+			dsKhachHang.get(i).setDiaChi(kH.getDiaChi());
+			dsKhachHang.get(i).setDiemTichLuy(kH.getDiemTichLuy());
+			dsKhachHang.get(i).setEmail(kH.getEmail());
+			dsKhachHang.get(i).setGioiTinh(kH.isGioiTinh());
+			dsKhachHang.get(i).setSDT(kH.getSDT());
+			dsKhachHang.get(i).setTen(kH.getTen());
+			return true;
+		}
+	}return false;
+}
+public KhachHang timKiemKhachHang(String ma) {
+	for(KhachHang temp : dsKhachHang) {
+		if(temp.getMa().equals(ma)) {
+			return temp;
+		}
+	}
+	return null;
+}
+public ArrayList<KhachHang> getDsKhachHang(){
+	return dsKhachHang;
+}
+public ArrayList<KhachHang> getDsKhachHangSapXepTheoDTL(){
+	ArrayList<KhachHang> list = dsKhachHang;
+	Collections.sort( list, new Comparator<KhachHang>() {
+
+		@Override
+		public int compare(KhachHang o1, KhachHang o2) {
+			// TODO Auto-generated method stub
+			
+			return (int)(o1.getDiemTichLuy()-o2.getDiemTichLuy());
+		}
+	});
+	return list;
+}
 }
