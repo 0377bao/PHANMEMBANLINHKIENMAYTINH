5e99c661ffbbb5a6e2d804eaebc2d3a8ce59e7e9
hovinhduy <hovinhduy0712@gmail.com>
2023-03-17 21:53:20 +0700

    them lop nhan vien va danh sach nhan vien

--- PHANMEMBANLINHKIENMAYTINH/src/model/DanhSachNhanVien.java
+++ PHANMEMBANLINHKIENMAYTINH/src/model/DanhSachNhanVien.java
@@ -1,5 +1,46 @@
 package model;
 
+import java.util.ArrayList;
+
+
 public class DanhSachNhanVien {
+    private ArrayList<NhanVien> danhSachNV;
+
+    public DanhSachNhanVien() {
+        danhSachNV = new ArrayList<>();
+    }
+
+    public boolean themNhanVien(NhanVien nv) {
+        if (danhSachNV.contains(nv)) {
+            return false;
+        }
+        danhSachNV.add(nv);
+        return true;
+    }
+
+    public boolean xoaNhanVien(NhanVien nv) {
+        return danhSachNV.remove(nv);
+    }
+
+    public NhanVien timNhanVien(String maNV) {
+        for (NhanVien nv : danhSachNV) {
+            if (nv.getMa().equals(maNV)) {
+                return nv;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<NhanVien> getDanhSachNV() {
+        return danhSachNV;
+    }
+    public boolean suaNhanVien(NhanVien nv) {
+        int index = danhSachNV.indexOf(nv);
+        if (index == -1) {
+            return false;
+        }
+        danhSachNV.set(index, nv);
+        return true;
+    }
 
 }
--- PHANMEMBANLINHKIENMAYTINH/src/model/NhanVien.java
+++ PHANMEMBANLINHKIENMAYTINH/src/model/NhanVien.java
@@ -1,5 +1,57 @@
 package model;
 
-public class NhanVien {
+import java.util.Objects;
+
+public class NhanVien extends ConNguoi{
+    private String name;
+    private String position;
+    private String cmnd;
+    private String taikhoan;
+    private String matkhau;
+
+    public NhanVien(String ma, String ten, String sDT, Boolean gioiTinh, String email, String diaChi, String position, String cmnd, String taikhoan, String matkhau) {
+        super(ma, ten, sDT, gioiTinh, email, diaChi);
+        this.position = position;
+        this.cmnd = cmnd;
+        this.taikhoan = taikhoan;
+        this.matkhau = matkhau;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getCmnd() {
+        return cmnd;
+    }
+
+    public String getTaikhoan() {
+        return taikhoan;
+    }
+
+    public String getMatkhau() {
+        return matkhau;
+    }
+
+    public void setMatkhau(String matkhau) {
+        this.matkhau = matkhau;
+    }
+
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof NhanVien)) {
+            return false;
+        }
+        NhanVien other = (NhanVien) obj;
+        return this.getMa().equals(other.getMa());
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(getMa());
+    }
 
 }
